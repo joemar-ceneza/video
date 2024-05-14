c98ef24786c4577906313694101163034f68b37f
joemar-ceneza <joemar.ceneza@gmail.com>
2024-05-14 20:47:06 +0800

    insert text

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -27,8 +27,9 @@ export default function VideoPlayer() {
   return (
     <main className="max-w-full w-screen mx-auto flex flex-col">
       <video
-        className="absolute top-0 left-0 w-full h-full object-cover -z-2"
+        className="absolute top-0 left-0 w-full h-full object-cover bg-slate-500 -z-2"
         controls
+        loop
         ref={videoRef}
         src={video}
         onTimeUpdate={handleTimeUpdate}></video>
@@ -36,6 +37,9 @@ export default function VideoPlayer() {
         <button onClick={togglePlay}>{isPlaying ? "Pause" : "Play"}</button>
         <span>Current Time: {Math.round(currentTime)}</span>
       </div> */}
+      <div className="flex items-center justify-center min-h-screen ">
+        <h1 className="text-white font-bold text-6xl z-10">Video Project</h1>
+      </div>
     </main>
   );
 }
